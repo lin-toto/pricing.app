dd18afb66879f5df071f4f891275c6cd6974b37e
Toto Lin <toto@linux.com>
2019-12-18 14:53:34 +0900

    Initial Commit

---
+++ Pricing/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pricing
+//
+//  Created by Toto Lin on 2019/12/18.
+//  Copyright © 2019 Toto Lin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pricing/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pricing
+//
+//  Created by Toto Lin on 2019/12/18.
+//  Copyright © 2019 Toto Lin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
