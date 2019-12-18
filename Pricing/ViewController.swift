4361024eb401a7970159adec91e571970dee51ac
Toto Lin <toto@linux.com>
2019-12-18 16:07:48 +0900

    Initial commit

--- Pricing/ViewController.swift
+++ Pricing/ViewController.swift
@@ -9,11 +9,22 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear() {
+        let window = self.view.window!
+        window.backgroundColor = NSColor.clear
+        window.isMovableByWindowBackground = false
+        window.isOpaque = false
+        window.setFrameTopLeftPoint(NSPoint(x: 30, y: NSScreen.main!.visibleFrame.size.height + 30))
+        
+        self.view.wantsLayer = true
+        self.view.layer!.backgroundColor = CGColor.white
+        self.view.layer!.cornerRadius = 5.0
+        self.view.layer!.masksToBounds = true
     }
 
     override var representedObject: Any? {
